a6d0c26fcab1cd62465c864271c742b3de887b1b
veetiengblom <Veeti_engblom@hotmail.com>
2024-04-14 23:25:18 +0300

    css fixes, modified table

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,8 +3,10 @@ import dataService from '../services/dataService';
 
 const Table = () => {
   const [games, setGames] = useState([]);
-  const [ascendingPower, setAscendingPower] = useState(true);
-  const [ascendingName, setAscendingName] = useState(true);
+  const [ascendingPower, setAscendingPower] = useState(false);
+  const [ascendingName, setAscendingName] = useState(false);
+  const [triangleOnGame, setTriangleOnGame] = useState(false);
+  const [showTriangle, setShowTriangle] = useState(false);
 
   const getGameData = () => {
     dataService.getAll().then((gameData) => {
@@ -12,78 +14,100 @@ const Table = () => {
     });
   };
 
+  useEffect(getGameData, []);
+
   const sortByPowerScore = (flip) => {
-    console.log('clicked, ascending:', flip);
+    setShowTriangle(true)
     if (flip) {
       const reverse = games.sort((a, b) => a.powerscore - b.powerscore);
-      console.log(reverse);
       setGames(reverse);
       setAscendingPower(flip);
+      setTriangleOnGame(false);
       return;
     }
+
     const reverse = games.sort((a, b) => b.powerscore - a.powerscore);
-    console.log(reverse);
     setGames(reverse);
     setAscendingPower(flip);
+    setTriangleOnGame(false);
     return;
   };
 
   const sortByName = (flip) => {
-    console.log('clicked, ascending:', flip);
+    setShowTriangle(true)
     if (flip) {
       const reverse = games.sort((a, b) =>
         a.name > b.name ? 1 : b.name > a.name ? -1 : 0
       );
-      console.log(reverse);
       setGames(reverse);
       setAscendingName(flip);
+      setTriangleOnGame(true);
       return;
     }
+
     const reverse = games.sort((a, b) =>
       b.name > a.name ? 1 : a.name > b.name ? -1 : 0
     );
-    console.log(reverse);
     setGames(reverse);
     setAscendingName(flip);
+    setTriangleOnGame(true);
     return;
   };
-
-  useEffect(getGameData, []);
 
   return (
     <div className='tableBody'>
       <h2>Top Grossing 5 Games</h2>
-
-      {/* Table */}
       <div className='tableWrapper'>
         <table className='gameTable'>
           <tbody>
             <tr className='gameTableRowHeader'>
               <th
                 className='tableItemHeader'
-                id='click'
+                id='powerscoreHeader'
                 onClick={() => sortByPowerScore(!ascendingPower)}
               >
-                POWERSCORE®
+                <div className='textContainerHeaderPowerscore'>
+                  <div>POWERSCORE®</div>
+                  {showTriangle ? (
+                    triangleOnGame ? null : ascendingPower ? (
+                      <div className='triangleUp'></div>
+                    ) : (
+                      <div className='triangleDown'></div>
+                    )
+                  ) : null}
+                </div>
               </th>
               <th
                 className='tableItemHeader'
                 id='center'
                 onClick={() => sortByName(!ascendingName)}
               >
-                Game
+                <div className='textContainerHeaderGame'>
+                  <div>Game</div>
+                  {showTriangle ? (
+                    triangleOnGame ? (
+                      ascendingName ? (
+                        <div className='triangleUp'></div>
+                      ) : (
+                        <div className='triangleDown'></div>
+                      )
+                    ) : null
+                  ) : null}
+                </div>
               </th>
               <th className='tableItemHeader'>App Store Link</th>
             </tr>
             {games?.map((game) => (
               <tr className='gameTableRow' key={game.name}>
-                <td className='tableItem'>{game.powerscore}</td>
+                <td className='tableItem' id='powerscore'>
+                  {game.powerscore}
+                </td>
                 <td className='tableItem' id='center' style={{ width: '60%' }}>
                   <div className='centerTableItem'>
                     <img src={game.iconUrl} alt='new' id='gameIcon' />
                     <div className='textContainer'>
-                      <div>{game.name}</div>
-                      <div>{game.artist}</div>
+                      <div className='gameName'>{game.name}</div>
+                      <div className='gameArtist'>{game.artist}</div>
                     </div>
                   </div>
                 </td>
